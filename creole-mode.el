a71d25bffa37447ca4a800ef273911550e3a661c
Nic Ferier <nic@ferrier.me.uk>
2013-01-03 01:36:44 +0000

    initial revision. doesn't do much.

---
+++ creole-mode.el
@@ -0,0 +1,70 @@
+;;; creole-mode.el --- a markup mode for creole
+
+;; Copyright (C) 2013  Nic Ferrier
+
+;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
+;; Keywords: hypermedia, wp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Markup creole files properly.
+;;
+;; This was prompted by this
+;; https://github.com/nicferrier/elwikicreole/issues/10 which
+;; referenced this:
+;; http://manuelp.github.com/emacs/2009/11/03/wikicreole-mode-emacs.html
+;;
+
+;;; Code:
+
+(defvar creole-mode-map nil
+  "Keymap for `creole-mode'.")
+
+(unless creole-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "\C-c\C-o" 'browse-url-at-point)
+    (setq creole-mode-map map)))
+
+(define-generic-mode 'creole-mode
+  nil ; comments
+  nil; keywords
+  '(("^\\(= \\)\\(.*?\\)\\($\\| =$\\)" . 'info-title-1)
+    ("^\\(== \\)\\(.*?\\)\\($\\| ==$\\)" . 'info-title-2)
+    ("^\\(=== \\)\\(.*?\\)\\($\\| ===$\\)" . 'info-title-3)
+    ("^\\(====+ \\)\\(.*?\\)\\($\\| ====+$\\)" . 'info-title-4)
+    ("\\[\\[.*?\\]\\]" . 'link)
+    ("\\[.*\\]" . 'link)
+    ("\\[b\\].*?\\[/b\\]" . 'bold)
+    ("//.*?//" . 'italic)
+    ("\\*\\*.*?\\*\\*" . 'bold)
+    ("_.*?_" . 'underline)
+    ("\\\\\\\\[ \t]+" . font-lock-warning-face)) ; font-lock list
+  '(".creole\\'"); auto-mode-alist
+  '((lambda ()
+      (require 'info)
+      (require 'goto-addr)
+      (require 'org)
+      (orgtbl-mode)
+      (use-local-map creole-mode-map))); function-list
+  "Creole-Wiki mode.
+
+Edit files written in WikiCreole form.
+
+\<KEYMAP>.")
+
+(provide 'creole-mode)
+
+;;; creole-mode.el ends here
